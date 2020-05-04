53bc2a794afa5056e0f17b026a10848b3119680b
EngSrayea <eng.nsrayea@gmail.com>
2020-05-04 23:12:06 +0300

    Day 2 CW

--- Day2/ContentView.swift
+++ Day2/ContentView.swift
@@ -9,13 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counters: [Int] = [0,0,0]
+    var Athkar: [String] = ["استغفرالله","سبحان الله وبحمده","الحمدلله"]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack(alignment: .trailing){
+            ThekrButton(counter: $counters[0], Thekr: Athkar[0])
+            ThekrButton(counter: $counters[1], Thekr: Athkar[1])
+            ThekrButton(counter: $counters[2], Thekr: Athkar[2])
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ThekrButton: View {
+    @Binding var counter : Int
+    var Thekr : String = " "
+    var body: some View {
+        HStack{
+            
+            Text(Thekr).font(.largeTitle)
+            Button(action: {
+                self.counter = self.counter + 1
+            }) {
+                Text("\(counter)    ")
+                    .font(.largeTitle)
+            }
+            .frame(width: 100, height: 100, alignment: .center)
+            .foregroundColor(.white)
+            .background(Color.gray)
+            .clipShape(Circle())
+            .padding()
+        }
     }
 }
